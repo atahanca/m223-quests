569105e197c65fc7cdc735338f5f46be0fb525ee
atahanca <atahan.ca@gmail.com>
2024-04-23 14:33:35 +0200

    created spring project

---
+++ sq3a/src/main/java/ch/wiss/sq3a/Sq3aApplication.java
@@ -0,0 +1,13 @@
+package ch.wiss.sq3a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sq3aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sq3aApplication.class, args);
+	}
+
+}
