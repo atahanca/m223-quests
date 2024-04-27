e0e4f99c34cb8effe4bc4dd04f65dcd6aad5b5d0
atahanca <atahan.ca@gmail.com>
2024-04-27 19:53:01 +0200

    feat(security) added SecurityFilterChain method in SecurityConfiguration.java

--- sq3a/src/main/java/ch/wiss/sq3a/config/SecurityConfiguration.java
+++ sq3a/src/main/java/ch/wiss/sq3a/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package ch.wiss.sq3a.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.User;
@@ -10,17 +11,20 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfiguration {
+public class SecurityConfiguration {
 
+
+    
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 // below are "hardcided" users into the system
-    
+    @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user = User.builder()
             .username("user")
